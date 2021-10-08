975ddc87f9608e39361816e2cd8c1318794840dc
nokomoto <69089446+nokomotc@users.noreply.github.com>
2021-10-08 14:18:10 +0300

    Added eof, deleted papumodule.action()
    Papumodule needs to know where to stop

--- example_module.py
+++ example_module.py
@@ -2,4 +2,5 @@ import paputalk as ppmd
 papumodule = ppmd.Module('example_module')
 @papumodule.command(['help']) # usage: m/example_module help
 async def help(message, cutted_text): # cutted_text is message.text cutted to be like this example: message.text - m/example_module help 1, cutted_text - 1
-    await papumodule.action(message).reply('This is an example module...')
+    await message.reply('This is an example module...')
+papumodule.eof()
