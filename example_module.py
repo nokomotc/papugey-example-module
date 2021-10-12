9091c96c6cee54282c0485286c224bfc78e6e6aa
nokomoto <69089446+nokomotc@users.noreply.github.com>
2021-10-12 21:55:05 +0300

    Update example_module.py

--- example_module.py
+++ example_module.py
@@ -1,4 +1,4 @@
-import paputalk as ppmd 
+import papumodule as ppmd 
 papumodule = ppmd.Module('example_module')
 @papumodule.command(['help']) # usage: m/example_module help
 async def help(message, cutted_text): # cutted_text is message.text cutted to be like this example: message.text - m/example_module help 1, cutted_text - 1
