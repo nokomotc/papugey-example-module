6f49c6945a6d188e96bb01a1194f5ac0f8b3c721
nokomoto <69089446+nokomotc@users.noreply.github.com>
2022-03-08 13:27:49 +0300

    Update example_module.py

--- example_module.py
+++ example_module.py
@@ -1,6 +1,6 @@
-import papumodule as ppmd 
-papumodule = ppmd.Module('example_module')
+import papumodule as ppmd # api
+papumodule = ppmd.Module('example_module') # here is the name of the module
 @papumodule.command(['help']) # usage: m/example_module help
 async def help(message, cutted_text): # cutted_text is message.text cutted to be like this example: message.text - m/example_module help 1, cutted_text - 1
-    await message.reply('This is an example module...')
-papumodule.eof()
+    await message.reply('This is an example module...') # aiogram syntax
+papumodule.eof() # end of file
