a2cd7d0dbe93ac0e810b4136925da395477d90ee
nokomoto <69089446+nokomotc@users.noreply.github.com>
2022-05-07 13:40:12 +0300

    totally reworked it

--- example_module.py
+++ example_module.py
@@ -1,6 +1,25 @@
-import papumodule as ppmd # api
-papumodule = ppmd.Module('example_module') # here is the name of the module
-@papumodule.command(['help']) # usage: m/example_module help
-async def help(message): # please note, that command structure is like this - m/{mod_name} {func_name} {args}
-    await message.reply('This is an example module...') # aiogram syntax
-papumodule.eof() # end of file
+from papumodule import Papumodule, register_handler
+class Module(Papumodule):
+    name = 'example_module' # m/example_module
+    requirements = [] # if you need something from pypi, just type it here
+    # requirements = ['numpy']
+    # but don't import the module via python's default import
+    
+    async def init_module(self): # not __init__, cause it needs to be async
+        # first arg is function, which is going to be called, after getting a message
+        # after first arg there are aiogram filters, BUT
+        # you cannot use command_prefix
+        await register_handler(self.help, commands=['help'])
+        await register_handler(self.hello, commands=['hello'])
+        await register_handler(self.unknown) # no handlers = m/example_module
+    async def help(message): # usage: m/example_module help
+        await message.reply('nope.') # aiogram
+    async def hello(message): 
+        await message.reply('Hello!')
+    async def unknown(message):
+        await message.reply('You sent me "m/example_module". What did you mean?')
+
+# and the structure of the command is:
+# m/{name} {command_name} {arguments}
+# like:
+# m/example_module hello_name Mike
